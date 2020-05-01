7408904c386a2279b1284eaf5512932df559a2b0
Jordan Contract <jordan59@optonline.net>
2020-04-30 23:54:44 -0400

    added files for custom hooks

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,41 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getAllEvents } from '../reducers/events';
 import Event from './Event';
 
-class EventList extends Component {
-    constructor(props) {
-        super(props);
-    }
+// class EventList extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
 
-    componentDidMount() {
-        this.props.getAllEvents();
-    }
+//     componentDidMount() {
+//         this.props.getAllEvents();
+//     }
 
-    render() {
-        const { events } = this.props;
-        return (
-            <div>
-                <h1>2020 Circuit/Festival Calendar</h1>
-                {events.map((event, index) => {
-                    return (
-                        <ul className="category" key={index}>
-                            <Event event={event} />
-                        </ul>
-                    );
-                })}
-            </div>
-        );
-    }
+//     render() {
+//         const { events } = this.props;
+//         return (
+
+//         );
+//     }
+// }
+
+// const mapStateToProps = state => ({
+//     events: state.event.eventList
+// });
+
+// const mapDispatchToProps = dispatch => ({
+//     getAllEvents: () => dispatch(getAllEvents())
+// });
+
+// export default connect(mapStateToProps, mapDispatchToProps)(EventList);
+
+export default function EventList(props) {
+    const { events } = props;
+    useEffect(() => {
+        props.getAllEvents();
+    }, [event]);
+    return (
+        <div>
+            <h1>2020 Circuit/Festival Calendar</h1>
+            {events.map((event, index) => {
+                return (
+                    <div className="category" key={index}>
+                        <Event event={event} />
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
-
-const mapStateToProps = state => ({
-    events: state.event.eventList
-});
-
-const mapDispatchToProps = dispatch => ({
-    getAllEvents: () => dispatch(getAllEvents())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(EventList);
